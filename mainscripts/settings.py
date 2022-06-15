3d89284fea4b7dcd8be24fc08ea33a2b72413266
Siddharth S <60936906+SiddharthSWolf@users.noreply.github.com>
2022-06-15 11:56:28 +0530

    been a long time bois

--- mainscripts/settings.py
+++ mainscripts/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from distutils.debug import DEBUG
 from pathlib import Path
 import os
 
